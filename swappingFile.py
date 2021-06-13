1fb43b5daa8d3c7997a9cc063223340e87f08014
Avani-3012 <65455563+Avani-3012@users.noreply.github.com>
2021-06-13 21:53:22 +0530

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,14 @@
+def swapFileData():
+    text = input("Enter the first file name")
+    
+    file1 = "firstSam.txt"
+    file2 = "secSam.txt"
+    with open(text, 'r') as file1
+    data_a = file1.read()
+    with open(text,'w')as 
+    file1.write = (data_b)
+    
+    with open(text, 'r') as file2
+       data_b = file2.read()
+    with open(text,'w')as file2
+       file2.write = (data_a)
